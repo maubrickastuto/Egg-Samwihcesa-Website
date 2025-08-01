568ec3511c80e4406b7ab75ba65ca6338bf99948
maubrickastuto <50073617+maubrickastuto@users.noreply.github.com>
2025-08-02 01:01:51 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.quickgibbonbob',
+  appName: 'quick-gibbon-bob',
+  webDir: 'dist'
+};
+
+export default config;
